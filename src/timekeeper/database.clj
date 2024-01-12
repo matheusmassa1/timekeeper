58c8fcecd03da47f66ee8e1ae8307d03a46b8375
matheusmassa1 <matheusmassa@protonmail.com>
2024-01-12 18:58:15 -0300

    feat: initial config for db

--- src/timekeeper/database.clj
+++ src/timekeeper/database.clj
@@ -1,1 +1,6 @@
-(ns timekeeper.database)
+(ns timekeeper.database
+  (:require [timekeeper.config :as config]
+            [next.jdbc :as jdbc]))
+
+(def db (jdbc/get-datasource (config/db-config)))
+
