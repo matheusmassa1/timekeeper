00a1ac50e782256a0d725024872ad6f759c60686
matheusmassa1 <matheusmassa@protonmail.com>
2024-02-03 13:49:12 -0300

    feat: rename gapi namespace

--- src/timekeeper/gapi.clj
+++ src/timekeeper/gapi.clj
@@ -1,4 +1,4 @@
-(ns timekeeper.gapi-auth
+(ns timekeeper.gapi
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
             [happy.oauth2 :as oauth2]
@@ -7,12 +7,12 @@
             [cheshire.core :as json]
             [timekeeper.config :refer [oauth-config gapi-scopes]]))
 
-(defn auth! []
-  (let [credentials @gapi-access-token
-        new-credentials (ocr/update-credentials (oauth-config) @gapi-access-token (gapi-scopes) nil)]
-    (set-access-token! new-credentials)
-    (oauth2/auth-header new-credentials)))
- 
+; (defn auth! [gapi-access-token]
+;   (let [credentials gapi-access-token
+;         new-credentials (ocr/update-credentials (oauth-config) gapi-access-token (gapi-scopes) nil)]
+;     (set-access-token! new-credentials)
+;     (oauth2/auth-header new-credentials)))
 
-(comment
+
+(comment 
   ,,,)
