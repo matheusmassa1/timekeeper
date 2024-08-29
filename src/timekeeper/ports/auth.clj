a5f3e26e1888b4474472ce8f4f2436a6270bea7b
matheusmassa1 <matheusmassa@protonmail.com>
2024-08-29 13:38:46 -0300

    feat: add ports for  auth

--- src/timekeeper/ports/auth.clj
+++ src/timekeeper/ports/auth.clj
@@ -1,6 +1,18 @@
 (ns timekeeper.ports.auth
   (:require [timekeeper.domain.user :refer [extract-login-info validate-user-login is-valid-user? generate-token-payload]]
-            [timekeeper.adapters.auth :refer [sign-token-adapter]]))
+            [timekeeper.adapters.auth :refer [sign-token-adapter]]
+            [buddy.hashers :as h]))
 
 (defn sign-token [sign-fn claims]
   (sign-fn claims))
+
+(defn login [find-fn auth-data]
+  (when (validate-user-login auth-data)
+    (let [user-data (find-fn auth-data)]
+      (when (seq user-data)
+        (let [hashed-password (h/derive (:password auth-data))
+              auth-data-with-hashed-password (assoc auth-data :password hashed-password)]
+          (when (is-valid-user? user-data auth-data-with-hashed-password)
+            (-> user-data
+                generate-token-payload
+                (sign-token sign-token-adapter))))))))
